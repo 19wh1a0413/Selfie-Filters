996bcd9f3ad7c78805cfc4388806ade62028e9de
Kummari Tejaswini <19wh1a12b7@bvrithyderabad.edu.in>
2021-05-12 15:24:20 +0000

    Update Selfie_filters_B612.py

--- Selfie_filters_B612.py
+++ Selfie_filters_B612.py
@@ -32,23 +32,18 @@ def prepare_data(df):
 
 
 def plot_data(img, face_points):
-    '''
-    Plot image and facial keypoints
-    Parameters:
-    --------------------
-    img: Image column value
-    face_point: Target column value
-    '''
-
     fig = plt.figure (figsize=(30, 30))
     ax = fig.add_subplot (121)
+
     ax.imshow (np.squeeze (img), cmap='gray')
     face_points = face_points * 48 + 48
+
     ax.scatter (face_points[0::2], face_points[1::2], marker='o', c='blue', s=25)
     plt.show ()
 
 df = pd.read_csv('training.csv')
 df=df.head(10)
+
 X_train, y_train =prepare_data(df)
 
 plot_data(X_train[3], y_train[3])
@@ -58,3 +53,31 @@ my_model = create_model()
 compile_model(my_model, optimizer = 'adam', loss = 'mean_squared_error', metrics = ['accuracy'])
 
 save_model(my_model, 'models/mm')
+
+def apply_filters(face_points, image_copy_1, image_name):
+    animal_filter = cv2.imread ("images/" + image_name, cv2.IMREAD_UNCHANGED)
+
+    for i in range (len (face_points)):
+        filter_width = 7 * (face_points[i][14] + 15 - face_points[i][18] + 15)
+        scale_factor = filter_width / animal_filter.shape[1]
+        sg = cv2.resize (animal_filter, None, fx=scale_factor, fy=scale_factor, interpolation=cv2.INTER_AREA)
+
+        width = sg.shape[1]
+        height = sg.shape[0]
+
+        # x coordinate = average x coordinate of eyes - width/2
+        # y coordinate = average y coordinate of eyes - height/2
+        x1 = int ((face_points[i][2] + 5 + face_points[i][0] + 5) / 2 - width / 2)
+        x2 = x1 + width
+
+        y1 = int ((face_points[i][2] - 65 + face_points[i][1] - 65) / 2 - height / 2)
+        y2 = y1 + height
+
+        # Create an alpha mask based on the transparency values
+        alpha_fil = np.expand_dims (sg[:, :, 3] / 255.0, axis=-1)
+        alpha_face =1.0 - alpha_fil
+
+        # Take a weighted sum of the image and the animal filter using the alpha values and (1- alpha)
+        image_copy_1[y1:y2, x1:x2] = (alpha_fil * sg[:, :, :3] + alpha_face * image_copy_1[y1:y2, x1:x2])
+
+    return image_copy_1
