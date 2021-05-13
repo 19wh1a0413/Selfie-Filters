a5b5cc175e64e0f825b1fd5e1106dab2b75a0934
Keerthichowdary Kommineni <19wh1a1290@bvrithyderabad.edu.in>
2021-05-13 05:09:05 +0000

    Update Selfie_filters_B612.py

--- Selfie_filters_B612.py
+++ Selfie_filters_B612.py
@@ -83,7 +83,6 @@ model = load_model('models/mm.h5')
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-#choice to launch filter
 global choice
 print ('Enter your choice filter to launch :')
 print('1 = Rabbit\n'
@@ -133,8 +132,7 @@ for (x, y, w, h) in faces:
         face_points[0::2] = face_points[0::2] * w / 2 + w / 2 + x
         face_points[1::2] = face_points[1::2] * h / 2 + h / 2 + y
         faces_keypoints.append (face_points)
-    
-# Plot facial keypoints on image
+
         for point in range (15):
             cv2.circle (image_copy, (face_points[2 * point], face_points[2 * point + 1]), 2, (255, 255, 0), -1)
 
@@ -142,43 +140,33 @@ for (x, y, w, h) in faces:
             if choice == 1:
                 s = apply_filters (faces_keypoints, image_copy_1, "s.png")
                 out.write (image_copy_1)
-                # Screen with the filter
                 cv2.imshow ('Screen with filter', s)
-
             elif choice == 2 :
                 dog = apply_filters(faces_keypoints, image_copy_1, "dog.png")
                 out.write(image_copy_1)
-                # Screen with the filter
                 cv2.imshow('Screen with filter', dog)
-#
             elif choice == 3 :
                 pig = apply_filters(faces_keypoints, image_copy_1, "pig.png")
                 out.write(image_copy_1)
-                # Screen with the filter
                 cv2.imshow('Screen with filter', pig)
             elif choice == 4:
                 fluffy = apply_filters(faces_keypoints, image_copy_1, "fluffy.png")
                 out.write(image_copy_1)
-                # Screen with the filter
                 cv2.imshow('Screen with filter', fluffy)
             elif choice == 5:
                 mask = apply_filters (faces_keypoints, image_copy_1, "mask.png")
                 out.write (image_copy_1)
-                # Screen with the filter
                 cv2.imshow ('Screen with filter', mask)
             else:
                 bear = apply_filters (faces_keypoints, image_copy_1, "bear.png")
                 out.write (image_copy_1)
-                # Screen with the filter
                 cv2.imshow ('Screen with filter', bear)
 
-        # Screen with facial keypoints
         cv2.imshow ('Screen with facial Keypoints predicted', image_copy)
     key = cv2.waitKey(1) & 0xff
-    if key == 27:  # The Esc key
+    if key == 27:  
         break
     if key % 256 == 32:
-        # SPACE to capture
         img_name = "opencv_frame_{}.png".format(img_counter)
         cv2.imwrite(img_name, image_copy_1)
         print("{} saved!!!".format(img_name))
