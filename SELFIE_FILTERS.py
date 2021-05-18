7cedcd60db4d24e32eb19734919d87487ceb3185
Keerthichowdary Kommineni <19wh1a1290@bvrithyderabad.edu.in>
2021-05-18 16:41:37 +0000

    Update SELFIE_FILTERS.py

--- SELFIE_FILTERS.py
+++ SELFIE_FILTERS.py
@@ -35,3 +35,27 @@ def create_cnn_model():
     model.add(layers.Dense(64, activation = 'relu'))
     model.add(layers.Dense(32, activation = 'relu'))
     return model
+
+def compile_cnn_model(model, optimizer, loss, metrics):
+    model.compile(optimizer = optimizer, loss = loss, metrics = metrics)
+
+def train_cnn_model(model, X_train, y_train):
+    return model.fit(X_train, y_train) #epochs = 100, batch_size = 200, verbose = 1, validation_split = 0.2)
+
+def load_cnn_model(fileName):
+    return models.load_model(fileName)
+
+def save_cnn_model(model, fileName):
+    model.save(fileName + '.h5')
+
+def train_test_split(df):
+    df['Image'] = df['Image'].apply(lambda x: np.fromstring(x, sep=' '))
+    df = df.dropna()
+    X = np.vstack(df['Image'].values) / 255
+    X = X.astype(np.float32)
+    X = X.reshape(-1, 96, 96, 1)
+    y = df[df.columns[:-1]].values
+    y = (y - 48) / 48
+    X, y = shuffle(X, y, random_state = 40)
+    y = y.astype(np.float32)
+    return X, y
