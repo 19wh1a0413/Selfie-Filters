1dc282f08c82b8068e2f806adac40f2c5d90a07e
Mothkur Suprathika <19wh1a05f7@bvrithyderabad.edu.in>
2021-05-18 15:45:41 +0000

    Update SELFIE_FILTERS.py

--- SELFIE_FILTERS.py
+++ SELFIE_FILTERS.py
@@ -5,3 +5,33 @@ import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
+
+def create_cnn_model():
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (5, 5), activation = 'relu', input_shape = (96, 96, 1)))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    model.add(layers.Conv2D(64, (3, 3), activation = 'relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Dropout(0.1))
+
+    model.add(layers.Conv2D(128, (3, 3), activation = 'relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Dropout(0.2))
+
+    model.add(layers.Conv2D(256, (3, 3), activation = 'relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(512, (3, 3), activation = 'relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Dropout(0.4))
+
+    model.add(layers.Flatten())
+
+    model.add(layers.Dense(512, activation = 'relu'))
+    model.add(layers.Dense(256, activation = 'relu'))
+    model.add(layers.Dense(128, activation = 'relu'))
+    model.add(layers.Dense(64, activation = 'relu'))
+    model.add(layers.Dense(32, activation = 'relu'))
+    return model
